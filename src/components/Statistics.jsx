d362ce41e797617675d2c3c846c04666dd71bfa0
Georg <georg@hainspiel33.de>
2023-02-11 12:27:43 +0100

    home v1

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import {
+  collection,
+  getCountFromServer,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+
+const Statistics = () => {
+  const [isFetching, setIsFetching] = useState(true);
+  const [imageCount, setImageCount] = useState(0);
+  const [objectsCount, setObjectsCount] = useState(0);
+
+  useEffect(() => {
+    const countAufnahmen = async () => {
+      try {
+        const collaufnahmen = collection(db, "aufnahmen");
+        const snapshot = await getCountFromServer(collaufnahmen);
+        setImageCount(snapshot.data().count);
+      } catch (error) {
+        toast.error("Da ging was schief");
+      }
+    };
+    const countObjects = async () => {
+        try {
+          const collobjects = collection(db, "objects");
+          const snapshot = await getCountFromServer(collobjects);
+          setObjectsCount(snapshot.data().count);
+        } catch (error) {
+          toast.error("Da ging was schief");
+        }
+      };
+
+    countAufnahmen();
+    countObjects();
+
+    setIsFetching(false);
+  }, []);
+
+  return (
+    <div>
+        <table>
+            <tr colspan="2">
+                <td>
+                <b>Statistik</b>
+                </td>
+            </tr>
+            <tr>
+                <td>Aufnahmen</td>
+                <td>{isFetching ? "..." : imageCount}</td>
+            </tr>
+            <tr>
+                <td>Objekte</td>
+                <td>{isFetching ? "..." : objectsCount}</td>
+            </tr>
+            <tr>
+                <td>Tutorials</td>
+                <td>0</td>
+            </tr>
+        </table>
+
+    </div>
+  );
+};
+
+export default Statistics;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
+import Andromeda from "../components/assets/jpg/Andromeda.jpg";
+import Statistics from "../components/Statistics";
 
 const Home = () => {
   return (
-    <div><h2>Home</h2></div>
-  )
-}
+    <div className="homescreen">
+      <Header />
+      <div className="toparea">
+        <div className="topareaimage">
+          <img className="mainimage" src={Andromeda} alt="astroschorsch" />
+        </div>
+      </div>
+      <div className="bottomarea">
+        <div className="maintitle">
+          <h2>Willkommen bei Astroschorsch</h2>
+          <i>"Vom Garten zu den Sternen"</i>
+        </div>
+        <Statistics />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
